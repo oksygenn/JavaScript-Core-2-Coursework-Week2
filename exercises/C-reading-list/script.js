a09d9ed4cf8a6cd6fdb21e53c8b1a7f0658ff3fc
Oksana Kostenko <kseniavlad31@gmail.com>
2021-02-22 18:01:47 +0000

    removes comments from C

--- exercises/C-reading-list/script.js
+++ exercises/C-reading-list/script.js
@@ -49,10 +49,3 @@ const books = [
 ];
 
 readingList(books);
-
-// - Iterate through the list of books.
-// - For each book, create a `<p>` element with the book title and author and append it to the page.
-// - Use a `<ul>` and `<li>` to display the books.
-// - Add an `<img>` to each book that links to a URL of the book cover.
-// - Change the style of the book depending on whether you have read it (green) or not (red).
-// - All of your HTML should go inside the `<div>` with the id **"content"**.
