d83a05657a26711c3361a88919ef751e2de790a9
Oksana Kostenko <kseniavlad31@gmail.com>
2021-02-22 14:20:50 +0000

    completes exercises/B

--- exercises/B-shopping-list/script.js
+++ exercises/B-shopping-list/script.js
@@ -1,5 +1,15 @@
-function shoppingList(arrayOfPeople) {
-  // Write your code here...
+function shoppingList(arrayOfItems) {
+  let divIdContent = document.querySelector("#content"); // find div with id "content"
+
+  let ulTag = document.createElement("ul"); // create ul tag
+
+  for (let item of arrayOfItems) {
+    let liTag = document.createElement("li"); // create li tag for every element of the array
+    liTag.innerText = item; // add every element of the array inside li tag
+    ulTag.appendChild(liTag); // append li tags to ul tag
+  }
+
+  divIdContent.appendChild(ulTag); // append ul to div
 }
 
 let shopping = ["Milk", "Bread", "Eggs", "A Dinosaur", "Cake", "Sugar", "Tea"];
