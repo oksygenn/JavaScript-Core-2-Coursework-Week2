b7acc9c65a3856a58d57b2007ec17ad11e61fafe
Oksana Kostenko <kseniavlad31@gmail.com>
2021-02-22 14:04:53 +0000

    completes exercises/A

--- exercises/A-list-of-names/script.js
+++ exercises/A-list-of-names/script.js
@@ -1,5 +1,21 @@
 function listOfNames(arrayOfPeople) {
   let content = document.querySelector("#content");
+
+  for (let person of arrayOfPeople) {
+    // create h1 and h2 tags for every element of arrayOfPeople
+    let h1Name = document.createElement("h1");
+    let h2Job = document.createElement("h2");
+
+    // get name and job of person from arrayOfPeople and put it inside h1 and h2 tags
+    h1Name.innerText = person.name;
+    h2Job.innerText = person.job;
+
+    // put h1 and h2 tags inside div with id "#content"
+    content.appendChild(h1Name);
+    content.appendChild(h2Job);
+  }
+
+  return content;
 }
 
 let people = [
