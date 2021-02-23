e8747eb850a12e8f4a535e1e809d29c82022459a
Oksana Kostenko <kseniavlad31@gmail.com>
2021-02-23 16:10:48 +0000

    completes exercises/D

--- exercises/D-todo-list/script.js
+++ exercises/D-todo-list/script.js
@@ -1,12 +1,29 @@
-function todoList(todos) {
-  // Write your code here...
+function todoList(todoList) {
+  let divIdContent = document.querySelector("#content");
+  let list = document.createElement("ul");
+
+  for (let item of todoList) {
+    let listItem = document.createElement("li");
+    listItem.innerText = item.todo;
+
+    listItem.addEventListener("click", () => {
+      if (listItem.style.textDecoration === "line-through") {
+        listItem.style.textDecoration = "none";
+      } else {
+        listItem.style.textDecoration = "line-through";
+      }
+    });
+
+    list.appendChild(listItem);
+  }
+  divIdContent.appendChild(list);
 }
 
 const todos = [
-  {todo: "wash the dishes"}, 
-  {todo: "walk the dog"}, 
-  {todo: "learn javascript"}, 
-  {todo: "go shopping"}
+  { todo: "wash the dishes" },
+  { todo: "walk the dog" },
+  { todo: "learn javascript" },
+  { todo: "go shopping" },
 ];
 
 todoList(todos);
