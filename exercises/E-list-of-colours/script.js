fd9bb6ec823096cc495e479c1314b9cf8768609c
Oksana Kostenko <kseniavlad31@gmail.com>
2021-02-23 16:11:13 +0000

    completes exercises/E

--- exercises/E-list-of-colours/script.js
+++ exercises/E-list-of-colours/script.js
@@ -1,5 +1,22 @@
 function listOfColours(colours) {
-  // Write your code here...
+  const divIdContent = document.querySelector("#content"); // find div with the id "content"
+  const selectEl = document.createElement("select"); // * Create a `<select>` element.
+  const pText = document.createElement("p"); // Create a `<p>` element.
+  pText.innerText = `You have selected: `;
+
+  divIdContent.appendChild(selectEl);
+  divIdContent.appendChild(pText);
+
+  for (colour of colours) {
+    const option = document.createElement("option");
+    option.innerText = colour;
+    selectEl.appendChild(option);
+  }
+
+  selectEl.addEventListener("change", () => {
+    pText.innerText = `You have selected: ${selectEl.value}`;
+    pText.style.color = selectEl.value;
+  });
 }
 
 const colours = ["red", "blue", "green", "yellow", "pink", "brown"];
